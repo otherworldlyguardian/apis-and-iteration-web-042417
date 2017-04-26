eb6524f2953d3a963ecffda36f7a4e28d1c674b9
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-04-26 12:02:14 -0400

    Program returns movies

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -13,27 +13,19 @@ def get_character_movies_from_api(character)
       end
     end
     character_hash = JSON.parse(RestClient.get(character_hash['next']))
-    binding.pry
   end
-
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
 end
 
 def parse_character_movies(films_hash)
+  films_hash.each.with_index(1) do |film, index|
+    film_info = JSON.parse(RestClient.get(film))
+    puts "#{index}. #{film_info["title"]}"
+  end
   # some iteration magic and puts out the movies in a nice list
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
-
   parse_character_movies(films_hash)
 end
 
